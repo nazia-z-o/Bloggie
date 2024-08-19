4f2986012d72949d9487a07294010ee2dcf56217
Nazia Zaman <naziaoyishee96@gmail.com>
2024-08-19 18:31:42 -0500

    Initializing new ASP.Net Web app project

---
+++ Blogg.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blogg.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
